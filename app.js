54a2d26cc311d7a68ffb42d27a4cc07bac767f71
Mayukh Chakraborty <mayukhchakraborty40@gmail.com>
2017-09-17 20:09:00 +0530

    upd

--- app.js
+++ app.js
@@ -4,18 +4,23 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/payments');
 
 var app = express();
 
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.disable('x-powered-by');
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/payments', payments);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
