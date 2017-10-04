7a62836ac5961d099ad8ac12e7f9916a813d8450
Mayukh Chakraborty <mayukhchakraborty40@gmail.com>
2017-10-04 14:46:10 +0530

    upd

--- paytm_utils/checksum.js
+++ paytm_utils/checksum.js
@@ -6,28 +6,15 @@ var crypto = require('crypto');
 
 //mandatory flag: when it set, only mandatory parameters are added to checksum
 
-function paramsToString(params, mandatoryflag) {
+function paramsToString(params) {
   var data = '';
   var tempKeys = Object.keys(params);
   tempKeys.sort();
-  tempKeys.forEach(function (key) {
-  var n = params[key].includes("REFUND"); 
-   var m = params[key].includes("|");  
-        if(n == true )
-        {
-          params[key] = "";
-        }
-          if(m == true)
-        {
-          params[key] = "";
-        }  
-    if (key !== 'CHECKSUMHASH' ) {
-      if (params[key] === 'null') params[key] = '';
-      if (!mandatoryflag || mandatoryParams.indexOf(key) !== -1) {
-        data += (params[key] + '|');
+  tempKeys.forEach(function (key) {  
+      if (key !== 'CHECKSUMHASH' ) {     
+          data += (params[key] + '|');     
       }
-    }
-});
+  });
   return data;
 }
 
