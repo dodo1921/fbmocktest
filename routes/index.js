897a102ab85da00c9790b1a6339d59c85fe46a84
Mayukh Chakraborty <mayukhchakraborty40@gmail.com>
2017-10-17 11:35:04 +0530

    upd

--- routes/index.js
+++ routes/index.js
@@ -925,7 +925,7 @@ function sendReport(recipientId ,curr_test){
 function startTest(recipientId, user) {
 
 
-  let qacount = 5, qbcount = 5, qa = [], qb = [], maxqa = 81, maxqb = 185, t, answer_queue='', question_queue='';
+  let qacount = 5, qbcount = 5, qa = [], qb = [], maxqa = 181, maxqb = 185, t, answer_queue='', question_queue='';
   let question_one, testid, test_taken=0;
 
   knex('tests').where({user_id: user.id}).count('user_id as i')
