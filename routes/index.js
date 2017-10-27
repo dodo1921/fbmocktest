79e5a61e04afb013650c6b7a01578ae2118cc609
Mayukh Chakraborty <mayukhchakraborty40@gmail.com>
2017-10-27 12:49:54 +0530

    upd

--- routes/index.js
+++ routes/index.js
@@ -1295,6 +1295,8 @@ function startTest(recipientId, user) {
 
           }).then( () => {
               sendTestQuestion(recipientId, question_one, testid, 1);
+              let d = new Date(); let t = d.getTime()+1200000;
+              setTimeout(processMessageExamMode(recipientId, user, t ), 1200000);
           }).catch( err => {
               console.log(err);
               sendMsgModeA(recipientId, err.name+' OMG');
