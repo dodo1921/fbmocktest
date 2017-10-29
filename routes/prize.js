5b636862efb9bf58f9f665018b9ba30792aaf3b2
Mayukh Chakraborty <mayukhchakraborty40@gmail.com>
2017-10-29 22:30:39 +0530

    upd

--- routes/prize.js
+++ routes/prize.js
@@ -47,7 +47,7 @@ router.get('/:recipientId', function(req, res) {
 						pending_share -= records[i].sharecount;
 					}
 
-					if(pending_share>=25)
+					if(pending_share>=2)
 						eligible = true;
 
 
