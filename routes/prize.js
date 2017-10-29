5e692ebe879c4cb6181d92c9a3823f91c4bb5a2f
Mayukh Chakraborty <mayukhchakraborty40@gmail.com>
2017-10-29 17:10:43 +0530

    upd

--- routes/prize.js
+++ routes/prize.js
@@ -12,39 +12,52 @@ var router = express.Router();
 router.get('/:recipientId', function(req, res) {  
 
 			let recipientId = req.params.recipientId;
+			let email = '';
+			let phone = '';
+			let total_share = 0;
+			let pending_share = 0;
+			let eligible = false;
+
 
 			knex('users').where({fbid: recipientId}).select()
 			.then( user => {
 
 				if(user.length>0){
 
-						let t, d;
-
-						knex('users').where('score', '>', user[0].score).orderBy('score', 'asc').limit(5)
-						.then( top => {
-
-							t = top.reverse();
-							return knex('users').where('score', '<', user[0].score).orderBy('score', 'desc').limit(10);
-
-						})
-						.then( down => {
-
-							return res.render('leaderboard.ejs', {
-								top: t,
-								me: user,
-								down,
-								recipientId: recipientId
-							});
-
-						})
-						.catch( err => {
-							return res.render('code_expired');
-						});
+					email = user[0].email;
+					phone = user[0].phone;
+						
+					return knex('users').where({ref_id: user[0].id}).count('ref_id as i')
 
 				}else
-					return res.render('code_expired');
+					throw new Error('Code Expired');
 
-			}).catch( err => {
+			})
+			.then(val => {
+
+				total_share = val[0].i;
+				return knex('refprize').where({fbid: recipientId}).select();
+
+			})
+			.then( records => {
+
+					pending_share = total_share;
+
+					for(let i=0; i< records.length; i++){
+						pending_share -= records[i].sharecount;
+					}
+
+					if(pending_share>=2)
+						eligible = true;
+
+
+					return res.render('share_prize', {
+						recipientId ,email, phone, total_share, pending_share, records, eligible
+					});
+
+
+			})
+			.catch( err => {
 				return res.render('code_expired');
 			});
 
